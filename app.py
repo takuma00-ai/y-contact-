22573c5c626a36a60514c15c13cde932ea401332
takuma00-ai <takuma._00icloud.com>
2026-05-13 03:05:36 +0000

    app.run

--- app.py
+++ app.py
@@ -724,8 +724,8 @@ def reset_all():
     return "全データ削除完了"
 
 if __name__=="__main__":
-    init_db()
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
 
     #conn=sqlite3.connect("user.db")
     db=conn.cursor()
